d250ec6fe0fade193a43b15862b9a58f248b66d2
Yash Patel <yashbpatel2004@gmail.com>
2024-04-02 18:55:28 -0400

    mysql database functionality

---
+++ src/mysql_connect.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+public class mysql_connect {
+    public static void connect(){
+        Connection con = null;;
+        //String login = System.getenv("login");
+        //String passwd = System.getenv("passwd");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/BMS","root","1502");
+            System.out.println("database connected");
+        } catch (SQLException e) {
+            System.out.println("JDBC driver not found");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
+    }
+}
