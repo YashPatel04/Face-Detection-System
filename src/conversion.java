6825793f07ad8b463da09219f70860b93649d6a9
Yash Patel <yashbpatel2004@gmail.com>
2024-04-09 22:28:39 -0400

    added real time face detection

---
+++ src/conversion.java
@@ -0,0 +1,17 @@
+import org.opencv.core.Mat;
+
+public class MattoBF extends Mat {
+    public BufferedImage matToBufferedImage(Mat mat) {
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (mat.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = mat.channels() * mat.cols() * mat.rows();
+        byte[] b = new byte[bufferSize];
+        mat.get(0, 0, b); // get all the pixels
+        BufferedImage image = new BufferedImage(mat.cols(), mat.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(b, 0, targetPixels, 0, b.length);
+        return image;
+    }
+}
